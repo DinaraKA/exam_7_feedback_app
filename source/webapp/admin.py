915159be9d34587877827f510284ac4452c52b87
DinaraKA <d.karybekova@gmail.com>
2019-10-19 12:38:24 +0600

    Added models, migrated, registered in admin and created dump

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from webapp.models import Poll, Choice, Answer
 
-# Register your models here.
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'question', 'created_at']
+    list_filter = ['created_at']
+    list_display_links = ['pk', 'question']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
+admin.site.register(Answer)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question=models.TextField(max_length=100, verbose_name='Question')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created date')
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    text=models.TextField(max_length=500, verbose_name='Text')
+    poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE, verbose_name='Poll')
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    poll = models.ForeignKey(Poll, related_name='poll_answers', on_delete=models.CASCADE, verbose_name='Poll')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created date')
+    choice = models.ForeignKey(Choice, related_name='choice_answers', on_delete=models.CASCADE, verbose_name='Choice')
+
+    def __str__(self):
+        return '{} | {}'.format(self.poll, self.choice)
+
